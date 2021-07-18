c740f69f0271867b8a637360a0c8758764be5311
snts.abode <snts.abode@gmail.com>
2021-07-18 13:54:26 +0200

    ganache config templates

---
+++ lib/files/configs/ganache.ts
@@ -0,0 +1,79 @@
+export const ForkChain = (
+  ethNode: string
+): string => `
+/* eslint-disable */
+const ganache = require('ganache-cli')
+const Web3 = require('web3')
+require('dotenv').config()
+
+if (!process.env.ETH_NODE_URL) throw new Error(
+  'ETH_NODE_URL undefined.'
+)
+
+const formatIntoMB = (param) => {
+  return Math.round(param / 1024 / 1024 * 100) / 100
+}
+
+/*setInterval(() => {
+  console.log('Memory')
+  console.log(line)
+  for (const [key, value] of Object.entries(process.memoryUsage())) {
+    console.log(key, ':', formatIntoMB(value), 'MB')
+  }
+  console.log()
+}, 300000)*/
+
+const server = ganache.server({
+  port: 9090,
+  default_balance_ether: 100,
+  fork: new Web3.providers.WebsocketProvider(${ethNode}),
+  ws: true,
+  debug: true,
+  vmErrorsOnRPCResponse: true,
+  verbose: true,
+  logger: console
+})
+
+const PORT = 7545
+const line = '===================='
+
+server.listen(PORT, onboot)
+
+function onboot(err, blockchain) {
+  if (err) {
+    console.error('Error booting ganache', err)
+    return process.exit(1)
+  }
+
+  let count = 1
+  console.log('Accounts')
+  console.log(line)
+
+  for (let [key, value] of Object.entries(blockchain.accounts)) {
+    const address = '0x' + value.secretKey.toString('hex')
+
+    console.log(count, 'Account:', key, '::: Private Key:', address)
+    count++
+  }
+
+  console.log()
+  console.log(line)
+  console.log()
+
+  console.log('Forked Chain')
+  console.log(line)
+  console.log('Block:       ', blockchain.blockchain.forkBlockNumber)
+  console.log('Network ID:  ', blockchain.net_version)
+  console.log('Time:        ', (blockchain.blockchain.startTime || new Date()).toString())  
+
+  console.log()
+  console.log(line)
+  console.log()
+  console.log('Ganache booted successfuly -> http://localhost:' + PORT)
+  console.log()
+  console.log(line)
+  console.log()
+}
+
+/* eslint-enable */
+`.trim()
