3cc707d158af0b5b2065fb0b0d26fb70afc9a57d
snts.abode <snts.abode@gmail.com>
2021-07-18 20:21:33 +0200

    ..

---
+++ lib/npm-packs.ts
@@ -0,0 +1,13 @@
+export const NPMPacks = {
+  UNISWAP: {
+    V2SDK: '@uniswap/v2-sdk'
+  },
+
+  DYDX: {
+    V3Client: '@dydxprotocol/v3-client'
+  },
+
+  BANCOR: {
+    SDK: '@bancor/sdk'
+  }
+}
