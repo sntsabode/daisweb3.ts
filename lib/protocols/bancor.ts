42e3aa6efb29ebc1dad12712861bf96afc25f47c
snts.abode <snts.abode@gmail.com>
2021-07-18 20:22:19 +0200

    more implementation

--- lib/protocols/bancor.ts
+++ lib/protocols/bancor.ts
@@ -1,42 +1,49 @@
-import { log, makeFile } from '../utils'
-import { interfacesPath } from './__paths__'
+import { colors, log, makeFile } from '../utils'
 import { resolve as pathResolve } from 'path'
 import { Bancor } from '../files/contracts/__contracts__'
 import { IContractImport } from '../daisconfig'
+import { NPMPacks } from '../npm-packs'
 
 export type SupportedImport =
-  'IBANCORNETWORK'
+  'IBANCORNETWORK' 
 
 /**
  * Assumes the `Bancor` directory is already created in the
- * `contract/libraries` and `contract/interfaces` directory
+ * `contract/libraries` and `contract/interfaces` directories
  * @param pack package being imported
  * @param solver solidity version
  */
 export async function BancorWriter(
+  dir: string,
   solver: string,
   ci: IContractImport
-): Promise<void> {
+): Promise<string> {
   switch(ci.pack.toUpperCase() as SupportedImport) {
     case 'IBANCORNETWORK':
-      return IBancorNetwork(solver)
-        .catch(e => { throw e })
+      return IBancorNetwork(dir, solver)
+        .then(
+          () => !ci.omitNpmPack ?
+            NPMPacks['BANCOR']['SDK'] : '',
+          e => { throw e }
+        )
 
     default:
-      return log.error(ci.pack, 'is not a vaild Bancor import')
+      log.error(ci.pack, 'is not a vaild', ...colors.red('Bancor'), 'import')
+      return ''
   }
 }
 
 async function IBancorNetwork(
+  dir: string,
   solver: string
-): Promise<void> {
-  return <unknown>Promise.all([
-    makeFile(pathResolve(interfacesPath + '/Bancor/IBancorNetwork.sol'),
-      Bancor.Interfaces.IBancorNetwork(solver)
-    ),
+): Promise<void[]> {
+  return Promise.all([
+    makeFile(pathResolve(
+      dir + '/contracts/interfaces/Bancor/IBancorNetwork.sol'
+    ), Bancor.Interfaces.IBancorNetwork(solver)),
 
-    makeFile(pathResolve(interfacesPath + '/Bancor/IContractRegistry.sol'),
-      Bancor.Interfaces.IContractRegistry(solver)
-    )
-  ]).catch(e => { throw e }) as void
+    makeFile(pathResolve(
+      dir + '/contracts/interfaces/Bancor/IContractRegistry.sol'
+    ), Bancor.Interfaces.IContractRegistry(solver))
+  ]).catch(e => { throw e })
 }
