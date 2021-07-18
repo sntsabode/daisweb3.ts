abf53657cf5b2d0bfb8ea7f7e401a49a89b02a3f
snts.abode <snts.abode@gmail.com>
2021-07-18 13:55:12 +0200

    config files

---
+++ lib/files/configs/__configs__.ts
@@ -0,0 +1,10 @@
+import * as TruffleConfigs from './truffle'
+export { TruffleConfigs as Truffle }
+import * as EslintConfigs from './eslint'
+export { EslintConfigs as Eslint }
+import * as GitConfigs from './git'
+export { GitConfigs as Git }
+import * as TSConfigs from './typescript'
+export { TSConfigs as TS }
+import * as GanacheConfigs from './ganache'
+export { GanacheConfigs as Ganache }
