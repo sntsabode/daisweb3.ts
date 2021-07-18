a9894f9b0c3837f95ceec8440c21b1cbb876ce4b
snts.abode <snts.abode@gmail.com>
2021-07-18 13:58:18 +0200

    contracts

---
+++ lib/files/contracts/__contracts__.ts
@@ -0,0 +1,81 @@
+interface IContractExportInterfaces<IT> {
+  readonly Interfaces: IT
+}
+
+interface IContractExportLibraries<LT> {
+  readonly Libraries: LT
+}
+
+/* DyDx */
+import * as DyDxInterfaces from './dydx/interfaces'
+import * as DyDxLibraries from './dydx/libraries'
+export const DyDx:
+  IContractExportInterfaces<
+    typeof DyDxInterfaces
+  > & IContractExportLibraries<
+    typeof DyDxLibraries
+  > = {
+  Interfaces: DyDxInterfaces,
+  Libraries: DyDxLibraries
+}
+/* /////\\\\\ */
+
+/* OpenZeppelin */
+import * as OpenZeppelinInterfaces from './openzeppelin/interfaces'
+export const OpenZeppelin:
+  IContractExportInterfaces<
+    typeof OpenZeppelinInterfaces
+  > = {
+  Interfaces: OpenZeppelinInterfaces
+}
+/* /////\\\\\ */
+
+/* Uniswap */
+import * as UniswapInterfaces from './uniswap/interfaces'
+export const Uniswap:
+  IContractExportInterfaces<
+    typeof UniswapInterfaces
+  > = {
+  Interfaces: UniswapInterfaces
+}
+/* /////\\\\\ */
+
+/* Kyber */
+import * as KyberInterfaces from './kyber/interfaces'
+export const Kyber:
+  IContractExportInterfaces<
+    typeof KyberInterfaces
+  > = {
+  Interfaces: KyberInterfaces
+}
+/* /////\\\\\ */
+
+/* Bancor */
+import * as BancorInterfaces from './bancor/interfaces'
+export const Bancor:
+  IContractExportInterfaces<
+    typeof BancorInterfaces
+  > = {
+  Interfaces: BancorInterfaces
+}
+/* /////\\\\\ */
+
+/* OneInch */
+import * as OneInchInterfaces from './oneinch/interfaces'
+export const OneInch:
+  IContractExportInterfaces<
+    typeof OneInchInterfaces
+  > = {
+  Interfaces: OneInchInterfaces
+}
+/* /////\\\\\ */
+
+/* Truffle */
+import * as TruffleLibraries from './truffle/libraries'
+export const Truffle:
+  IContractExportLibraries<
+    typeof TruffleLibraries
+  > = {
+  Libraries: TruffleLibraries
+}
+/* /////\\\\\ */
