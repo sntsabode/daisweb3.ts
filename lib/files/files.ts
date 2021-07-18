61a8b5639853bf4cb42693175a30882b33b4ceaf
snts.abode <snts.abode@gmail.com>
2021-07-18 15:09:21 +0200

    files

---
+++ lib/files/files.ts
@@ -0,0 +1,6 @@
+import * as Contracts from './contracts/__contracts__'
+export { Contracts }
+import * as ABIs from './abis/__abis__'
+export { ABIs }
+import * as Configs from './configs/__configs__'
+export { Configs }
