2634890c9e77df845c3142f424bc011de58c5360
snts.abode <snts.abode@gmail.com>
2021-07-19 18:59:37 +0200

    added `buildABIFile` and friends

--- lib/lib.ts
+++ lib/lib.ts
@@ -5,7 +5,7 @@ import { IContractImport, IDaisConfig, SupportedNetwork, SupportedProtocol, Supp
 import fs from 'fs'
 import { BancorWriter } from './protocols/bancor'
 import { DyDxWriter } from './protocols/dydx'
-import { IWriterReturn } from './protocols/__imports__'
+import { IABIReturn, IAddressReturn, IWriterReturn } from './protocols/__imports__'
 
 class ProtocolFileWriter {
   static readonly instance = new ProtocolFileWriter()
@@ -22,31 +22,112 @@ class ProtocolFileWriter {
     return protocolObject
   })()
 
+  readonly #addresses: {
+    [protocol in SupportedProtocol]: IAddressReturn[]
+  } & {
+    ERROR: IAddressReturn[]
+  } = (function () {
+    const obj = <
+      { [protocol in SupportedProtocol]: IAddressReturn[] } &
+      { ERROR: IAddressReturn[] }
+    >{}
+
+    for (const protocol of SupportedProtocolsArray)
+      obj[protocol] = []
+    obj.ERROR = []
+    return obj
+  })()
+
+  readonly #abis: {
+    [protocol in SupportedProtocol]: IABIReturn[]
+  } & {
+    ERROR: IABIReturn[]
+  } = (function () {
+    const obj = <
+      { [protocol in SupportedProtocol]: IABIReturn[] } &
+      { ERROR: IABIReturn[] }
+    >{}
+
+    for (const protocol of SupportedProtocolsArray)
+      obj[protocol] = []
+    obj.ERROR = []
+    return obj
+  })()
+
+  /**
+   * Main contract writer entry point
+   * @param dir 
+   * @param contractImports 
+   * @param solver 
+   * @param net 
+   * @returns An array of dependencies meant to be installed,
+   * declared in the `contractImports` section of the `.daisconfig`
+   * file
+   */
   readonly main = async (
     dir: string,
     contractImports: IContractImport[],
     solver: string,
     net: SupportedNetwork | 'all'
-  ): Promise<IWriterReturn[]> => this.#makeBaseDirs(dir)
+  ): Promise<string[]> => this.#makeBaseDirs(dir)
     .then(() => this.#work(
     solver, contractImports, dir, net
-  ), e => { throw e })
+  ), e => { throw e }).then(
+    (val) => Promise.all([
+      this.#buildABIFile(dir)
+    ]).then(
+      () => val,
+      e => { throw e }
+    ),
+
+    e => { throw e }
+  )
 
   readonly #work = async (
     solver: string,
     contractImports: IContractImport[],
     dir: string,
     net: SupportedNetwork | 'all'
-  ): Promise<IWriterReturn[]> => Promise.all(contractImports.map(
-    ci => this.protocols[(() => {
-      const protocol = ci.protocol.toUpperCase() as SupportedProtocol
-      if (!this.protocols[protocol])
-        return 'ERROR' 
-      return protocol
-    })() as SupportedProtocol | 'ERROR'](
-      dir, solver, net, ci
-    ).catch(e => { throw e })
+  ): Promise<string[]> => Promise.all(contractImports.map(
+    ci => {
+      let protocol = <SupportedProtocol | 'ERROR'>ci.protocol.toUpperCase()
+      protocol = !this.protocols[protocol] ? 'ERROR' : protocol
+      return this.protocols[protocol](
+        dir, solver, net, ci
+      ).then(val => {
+        this.#abis[protocol].push(
+          ...val.ABIs
+        )
+  
+        this.#addresses[protocol].push(
+          ...val.Addresses
+        )
+  
+        return val.Pack
+      }, e => { throw e })
+    }
   ))
+
+  readonly #buildABIFile = async (
+    dir: string
+  ) => {
+    let ABIfile = ''
+    for (const [protocol, abis] of Object.entries(this.#abis)) {
+      if (
+        abis.length === 0
+        || protocol === 'ERROR'
+      ) continue
+      
+      ABIfile += `\nnamespace ${protocol} {`
+      for (const abi of abis)
+        ABIfile += '\n  ' + abi.ABI
+
+      ABIfile += '\n}'
+    }
+
+    return makeFile(pathResolve(dir + '/lib/addresses.ts'), ABIfile.trim())
+      .catch(e => { throw e })
+  }
 
   readonly #bancor = async (
     dir: string,
@@ -122,7 +203,9 @@ class ProtocolFileWriter {
     dir: string
   ): Promise<void[]> => Promise.all([
     makeDir(pathResolve(dir + '/contracts/interfaces')),
-    makeDir(pathResolve(dir + '/contracts/libraries'))
+    makeDir(pathResolve(dir + '/contracts/libraries')),
+    makeDir(pathResolve(dir + '/lib')),
+    makeDir(pathResolve(dir + '/migrations'))
   ]).catch(e => { throw e })
 }
 
