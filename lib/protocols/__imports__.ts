e92e3cc2652d6a4ce4f4b01fd6cc73fcd5320fd6
snts.abode <snts.abode@gmail.com>
2021-07-19 17:21:18 +0200

    file writer interface

--- lib/protocols/__imports__.ts
+++ lib/protocols/__imports__.ts
@@ -1,9 +1,36 @@
-import { IContractImport } from '../daisconfig'
+import { SupportedNetwork } from '../daisconfig'
 
+export interface IIndividualWriterReturn {
+  Addresses: IAddressReturn[]
+  ABIs: IABIReturn[]
+}
+
+export interface IWriterReturn extends IIndividualWriterReturn {
+  Pack: string
+}
+
+/**
+ * Interface for the functions that write the required smart
+ * contract files.
+ * 
+ */
 export type TImports<K> = {
   readonly [protocol in keyof K]: (
     dir: string,
     solver: string,
-    ci: IContractImport
-  ) => Promise<void[]>
+    net: SupportedNetwork | 'all',
+    abi: boolean,
+    pack: string
+  ) => Promise<IIndividualWriterReturn>
+}
+
+export interface IABIReturn {
+  ContractName: string
+  ABI: string
+}
+
+export interface IAddressReturn {
+  NET: SupportedNetwork
+  ContractName: string
+  Address: string
 }
