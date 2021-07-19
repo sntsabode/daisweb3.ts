84e6101efa41a7f7e15ea57253e8cbb092d73607
snts.abode <snts.abode@gmail.com>
2021-07-19 17:19:38 +0200

    addresses file

---
+++ lib/addresses.ts
@@ -0,0 +1,27 @@
+export const Addresses = {
+  BANCOR: {
+    ContractRegistry: {
+      MAINNET: '0x52Ae12ABe5D8BD778BD5397F99cA900624CfADD4',
+      KOVAN: '',
+      ROPSTEN: '0xFD95E724962fCfC269010A0c6700Aa09D5de3074'
+    },
+
+    ETH: '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE'
+  },
+
+  DYDX: {
+    ISoloMargin: {
+      MAINNET: '0x1E0447b19BB6EcFdAe1e4AE1694b0C3659614e4e',
+      KOVAN: '',
+      ROPSTEN: ''
+    }
+  },
+
+  KYBER: {
+    IKyberNetworkProxy: {
+      MAINNET: '0x9AAb3f75489902f3a48495025729a0AF77d4b11e',
+      KOVAN: '0xc153eeAD19e0DBbDb3462Dcc2B703cC6D738A37c',
+      ROPSTEN: '0xd719c34261e099Fdb33030ac8909d5788D3039C4'
+    }
+  }
+}
