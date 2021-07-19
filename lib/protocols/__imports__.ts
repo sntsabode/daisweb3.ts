4fe3158219eefcc24eabf6a44dcf7cf9b7678395
snts.abode <snts.abode@gmail.com>
2021-07-19 21:15:20 +0200

    refactor

--- lib/protocols/__imports__.ts
+++ lib/protocols/__imports__.ts
@@ -9,19 +9,23 @@ export interface IWriterReturn extends IIndividualWriterReturn {
   Pack: string
 }
 
+export type IIndividualWriterFunc = (
+  dir: string,
+  solver: string,
+  net: SupportedNetwork | 'all',
+  abi: boolean,
+  pack: string
+) => Promise<IIndividualWriterReturn>
+
 /**
  * Interface for the functions that write the required smart
  * contract files.
  * 
  */
-export type TImports<K> = {
-  readonly [protocol in keyof K]: (
-    dir: string,
-    solver: string,
-    net: SupportedNetwork | 'all',
-    abi: boolean,
-    pack: string
-  ) => Promise<IIndividualWriterReturn>
+export type TImports<K extends string> = {
+  readonly [protocol in K]: IIndividualWriterFunc
+} & {
+  ERROR: IIndividualWriterFunc
 }
 
 export interface IABIReturn {
