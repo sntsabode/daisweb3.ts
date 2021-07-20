7826929ea5c8f890bdd85c6f283e4b3ea7d06b6b
snts.abode <snts.abode@gmail.com>
2021-07-20 12:02:33 +0200

    OneInchAudit address

--- lib/addresses.ts
+++ lib/addresses.ts
@@ -1,4 +1,7 @@
 export const Addresses = {
+  /**
+   * Bancor contract addresses
+   */
   BANCOR: {
     ContractRegistry: {
       MAINNET: '0x52Ae12ABe5D8BD778BD5397F99cA900624CfADD4',
@@ -9,6 +12,9 @@ export const Addresses = {
     ETH: '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE'
   },
 
+  /**
+   * DyDx contract addresses
+   */
   DYDX: {
     ISoloMargin: {
       MAINNET: '0x1E0447b19BB6EcFdAe1e4AE1694b0C3659614e4e',
@@ -17,11 +23,25 @@ export const Addresses = {
     }
   },
 
+  /**
+   * Kyber contract addresses
+   */
   KYBER: {
     IKyberNetworkProxy: {
       MAINNET: '0x9AAb3f75489902f3a48495025729a0AF77d4b11e',
       KOVAN: '0xc153eeAD19e0DBbDb3462Dcc2B703cC6D738A37c',
       ROPSTEN: '0xd719c34261e099Fdb33030ac8909d5788D3039C4'
+    }
+  },
+
+  /**
+   * OneInch Contract addresses
+   */
+  ONEINCH: {
+    OneSplitAudit: {
+      MAINNET: '0xC586BeF4a0992C495Cf22e1aeEE4E446CECDee0E',
+      KOVAN: '',
+      ROPSTEN: ''
     }
   }
 }
