965b9317c0c85ce4b935212b73b77ae75bc68c33
snts.abode <snts.abode@gmail.com>
2021-08-13 06:25:49 +0200

    quick write feature

--- lib/quickwrite.ts
+++ lib/quickwrite.ts
@@ -2,7 +2,11 @@
 
 import { SupportedNetwork, SupportedProtocol } from './daisconfig'
 import { ProtocolFileWriter } from './protocol-writer'
-import { colors, log } from './utils'
+import { IABIReturn, IAddressReturn } from './protocols/__imports__'
+import { colors, log, makeDir, makeFile } from './utils'
+import { resolve as pathResolve } from 'path'
+import { npminit, runInstallCommands } from './lib'
+import { readFile } from 'fs'
 
 export type SupportedArgsLength = '1' | '2' | '3' | '4' | '5' | '6'
 
@@ -42,7 +46,11 @@ const supportedProtocols: {
 async function quickWrite(args: string[], dir: string) {
   const protocol = assertProtocol(args[0])
   const pack = assertPack(protocol, args[1])
-  const [abi, omitNpmPack, solver] = [args[2], args[3], args[4]]
+  const [abi, omitNpmPack, solver] = [
+    boolParams(args[2], 'false'),
+    boolParams(args[3], 'false'),
+    args[4]
+  ]
   const network = assertNetwork(args[5])
 
   const res = await ProtocolFileWriter.instance.protocols[protocol](
@@ -52,12 +60,123 @@ async function quickWrite(args: string[], dir: string) {
     {
       protocol,
       pack,
-      abi: abi ? true : false,
-      omitNpmPack: omitNpmPack ? true : false
+      abi,
+      omitNpmPack
     }
   )
 
-  console.log(res)
+  await writeABIFiles(res.ABIs, dir).catch(e => {
+    throw e
+  })
+
+  const packjsonExists = await findFile(dir, 'package.json').catch(e => {
+    throw e
+  })
+
+  if (!packjsonExists && res.Pack.length > 0)
+    await npminit(true).catch(e => {
+      throw e
+    })
+
+  if (res.Pack.length > 0)
+    await runInstall(res.Pack, dir).catch(e => {
+      throw e
+    })
+
+  logABIImports(res.ABIs, protocol)
+  logAddresses(res.Addresses, protocol)
+}
+
+async function runInstall(deps: string[], dir: string) {
+  return runInstallCommands('yarn', false, deps).then(async yarn => {
+    // eslint-disable-next-line
+    if (yarn!.code !== 0) {
+      const yarnlockExists = await findFile(dir, 'yarn.lock')
+      if (!yarnlockExists)
+        return runInstallCommands('npm', false, deps).catch(e => {
+          throw e
+        })
+
+      log.warning('Found', colors.cyan('yarn.lock')[0], 'file. Aborting npm i')
+    }
+
+    return yarn
+  })
+}
+
+async function findFile(dir: string, file: string) {
+  return new Promise<boolean>(resolve => {
+    readFile(pathResolve(dir, file), err => {
+      if (err) resolve(false)
+      resolve(true)
+    })
+  })
+}
+
+async function writeABIFiles(abis: IABIReturn[], dir: string) {
+  const Promises = () =>
+    Promise.all(
+      abis.map(abi =>
+        makeFile(
+          pathResolve(`${dir}/lib/__abis__/abis/${abi.ContractName}.json`),
+          abi.ABI
+        )
+      )
+    )
+
+  return Promises().catch(e => {
+    if (!e.errno) throw e
+    if (e.errno !== -2) throw e
+
+    return makeDir(pathResolve(`${dir}/lib/__abis__/abis/`)).then(
+      () => Promises(),
+      e => {
+        throw e
+      }
+    )
+  })
+}
+
+function logABIImports(abis: IABIReturn[], protocol: SupportedProtocol) {
+  if (abis.length === 0) return
+
+  const [export_, const_, eq, require_] = colors.magenta(
+    'export',
+    'const',
+    '=',
+    'require'
+  )
+  let ABIfile = `${export_} ${const_} ${protocol}_ABIs ${eq} {`
+
+  for (const abi of abis) {
+    const [pathToABI_] = colors.blue(`'./abis/${abi.ContractName}.json'`)
+    ABIfile += `\n  ${abi.ContractName}: ${require_}(${pathToABI_})`
+  }
+
+  ABIfile += `\n}`
+
+  console.log()
+  console.log(ABIfile)
+}
+
+function logAddresses(
+  addresses: IAddressReturn[],
+  protocol: SupportedProtocol
+) {
+  const [export_, const_, eq] = colors.magenta('export', 'const', '=')
+  let AddressesFile = `${export_} ${const_} Addresses ${eq} {`
+  AddressesFile += `\n  ${protocol}: {`
+
+  for (const address of addresses) {
+    const [address_] = colors.blue(`'${address.Address}'`)
+    AddressesFile += `\n    ${address.ContractName}_${address.NET}: ${address_}`
+  }
+
+  AddressesFile += `\n  }`
+  AddressesFile += `\n}`
+
+  console.log()
+  console.log(AddressesFile)
 }
 
 /**
@@ -150,17 +269,7 @@ function protocolPackAndAbi(args: string[]) {
   )
 }
 
-/**
- * Called if only one or two arguments are passed into the
- * daisweb3 command
- * @param args
- * @returns
- */
-function protocolOnly(args: string[]) {
-  const protocol = assertProtocol(args[0])
-  return protocolOnly_(protocol)
-}
-function protocolOnly_(protocol: SupportedProtocol) {
+function logSupportedImports(protocol: SupportedProtocol) {
   const [prot] = colors.green(protocol)
   log.withbox(`
     You have entered the ${prot} protocol.
@@ -170,14 +279,22 @@ function protocolOnly_(protocol: SupportedProtocol) {
       protocol_ => `\n${colors.green(protocol_)[0]}`
     )}
   `)
+}
 
-  log.success(`
-    Run ${colors.cyan('daisweb3')[0]} <${protocol}> <${supportedProtocols[
-    protocol
-  ].map((prot_, i) =>
-    i === 0 ? prot_ : ` | ${prot_}`
-  )}> <abi | false> <omitNpmPack | false> <0.8.6> <SupportedNetwork | all>
-  `)
+/**
+ * Called if only one or two arguments are passed into the
+ * daisweb3 command
+ * @param args
+ * @returns
+ */
+function protocolOnly(args: string[]) {
+  const protocol = assertProtocol(args[0])
+  logSupportedImports(protocol)
+}
+
+function boolParams(omitNpmPack: string, param: string) {
+  if (omitNpmPack === param) return false
+  return true
 }
 
 function assertNetwork(net: string) {
@@ -191,8 +308,11 @@ function assertNetwork(net: string) {
 function assertPack(prot: SupportedProtocol, pack: string) {
   return !supportedProtocols[prot].includes(pack)
     ? (() => {
+        logSupportedImports(prot)
         throw new Error(
-          `You have entered an unsupported ${colors.green(prot)[0]} import`
+          `You have entered an unsupported ${
+            colors.green(prot)[0]
+          } import. See above for supported imports.`
         )
       })()
     : pack
